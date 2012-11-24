f4d85dd69f82cb3e15226cd65f79487de6a103e7
Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
2012-11-24 21:29:54 +0100

    Add Status query builder

--- Repository/StatusRepository.php
+++ Repository/StatusRepository.php
@@ -12,4 +12,46 @@ use Doctrine\ORM\EntityRepository;
  */
 class StatusRepository extends EntityRepository
 {
+    /**
+     * getDiscrStatusQueryBuilder
+     *
+     * @param $discr string the discriminant
+     * @return QueryBuilder
+     */
+    public function getDiscrStatusQueryBuilder($discr)
+    {
+        $qb = $this->createQueryBuilder('s')
+            ->where('s.discr = :discr')
+            ->setParameter('discr', $discr)
+            ->orderBy('s.value', 'ASC')
+        ;
+
+        return $qb;
+    }
+
+    /**
+     * getDiscrStatusQuery
+     *
+     * @param $discr string the discriminant
+     * @return Query
+     */
+    public function getDiscrStatusQuery($discr)
+    {
+        $qb = $this->getDiscrStatusQueryBuilder($discr);
+
+        return is_null($qb) ? $qb : $qb->getQuery();
+    }
+
+    /**
+     * getDiscrStatus
+     *
+     * @param $discr string the discriminant
+     * @return DoctrineCollection
+     */
+    public function getDiscrStatus($discr)
+    {
+        $q = $this->getDiscrStatusQuery($discr);
+
+        return is_null($q) ? array() : $q->getResult();
+    }
 }

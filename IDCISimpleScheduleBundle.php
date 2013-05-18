f21c97fdf7d31d512a2b5aa547ae84696bcb9d22
Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
2013-05-19 00:42:18 +0300

    Fix wrong use !

--- IDCISimpleScheduleBundle.php
+++ IDCISimpleScheduleBundle.php
@@ -9,10 +9,7 @@
 
 namespace IDCI\Bundle\SimpleScheduleBundle;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-use IDCI\Bundle\ExporterBundle\DependencyInjection\IDCIExporterExtension;
 
 class IDCISimpleScheduleBundle extends Bundle
 {
